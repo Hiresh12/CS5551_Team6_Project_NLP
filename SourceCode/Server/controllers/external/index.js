ef6ba151f815e5dcf5764ee0d92174b1fa5c31e7
Hiresh12 <42656868+Hiresh12@users.noreply.github.com>
2018-10-26 17:28:45 -0500

    nlp

---
+++ SourceCode/Server/controllers/external/index.js
@@ -0,0 +1,24 @@
+var request = require('request');
+module.exports = function(app) {
+    var query='https://language.googleapis.com/v1beta2/documents:analyzeSyntax?key=AIzaSyA7f9wgc1XwprL1hSPWFaBotvttChr24yU';
+    //var url = 'https://api.wunderground.com/api/36b799dc821d5836/hourly/q/MO/kansas%20city.json';
+    app.post('/hiresh/nlp', function(req, res) {
+        console.log(req.query);
+        console.log('1');
+        res.contentType("application/json");
+       request.post({
+               url: query,
+               method: 'POST',
+               json: {
+
+                   "document" :  {
+                       "type":"PLAIN_TEXT",
+                       "content" : req.query.text
+                   }
+                   ,
+                   "encodingType" : "None"
+               }
+           }
+       ).pipe(res);
+    });
+}
