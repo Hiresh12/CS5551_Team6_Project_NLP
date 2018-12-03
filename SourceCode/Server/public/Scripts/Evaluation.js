d9cc05b689aeab6dc994df31a0db41b1e8a88d32
Hiresh12 <42656868+Hiresh12@users.noreply.github.com>
2018-12-02 23:54:47 -0600

    answer eval

--- SourceCode/Server/public/Scripts/Evaluation.js
+++ SourceCode/Server/public/Scripts/Evaluation.js
@@ -1,5 +1,5 @@
 var student;var tutor;var keyword;var keys='';var keywords;var result;var keyNotinAns;var ansNotinKey;
-    var foundinAns;var useranswer;
+    var foundinAns;var useranswer;var foundinKey;
     var synonyms={
         synonyms:{}
     };
@@ -14,8 +14,6 @@ var student;var tutor;var keyword;var keys='';var keywords;var result;var keyNot
 var textAnalyze = function($scope,$http,qid) {
     var query='https://api.mlab.com/api/1/databases/nlp_database/collections/questions?apiKey=lSMpIYO_9GB-Gnslwjb-KPeyA4fc4hxb';
     $http.get(query+'&q={"question_id":'+qid+'}').then(function(data){
-        console.log(data);
-        console.log('check');
         Question=data.data[0];
         AnswerEval($scope,$http,qid);
     });
@@ -34,43 +32,49 @@ function AnswerEval($scope,$http,qid){
 }
     function Result(student,tutor,$http)
     {
-        //console.log(tutor);
-        //console.log(Question.keywords);
         keywords = alasql('SELECT LOWER(keywords.key) as a,LOWER(tutor.text.content) as b,LOWER(tutor.partOfSpeech.tag) as c,keywords.score as d \
-         ,tutor.lemma as lemma,keywords.score as score\
+         ,tutor.lemma as lemma,keywords.score as score,tutor.dependencyEdge.headTokenIndex as actlIndex,tutor.dependencyEdge.label as actlLabel\
          FROM ? AS keywords JOIN ? AS tutor ON LOWER(keywords.key) LIKE "%"+'+'LOWER(tutor.text.content)'+'+"%"\
          OR " "+'+'LOWER(keywords.key)'+'+" " LIKE "% "+'+'LOWER(tutor.lemma)'+'+" %"',
             [Question.keywords,tutor]);
-        console.log(keywords);
-        //console.log(student);
-        result = alasql('SELECT student.text.content as ans,keywords.a as key,keywords.d as score \
+
+        result = alasql('SELECT student.text.content as ans,keywords.a as key,keywords.d as score,student.dependencyEdge.headTokenIndex as ansIndex,keywords.actlIndex as actlIndex \
+        ,student.dependencyEdge.label as anslabel,keywords.actlLabel as actlLabel,"" as actvalue,"" as ansvalue\
          FROM ? AS student JOIN ? AS keywords ON LOWER(student.text.content) = keywords.b\
          OR LOWER(student.lemma) = keywords.b',
             [student, keywords]);
-        //console.log(result);
+
+        for(var i=0;i<result.length;i++)
+        {
+            result[i].actvalue=tutor[result[i].actlIndex].text.content;
+            result[i].ansvalue=student[result[i].ansIndex].text.content
+        }
 
         keyNotinAns = alasql('COLUMN OF SELECT keywords.b \
          FROM ? AS student RIGHT JOIN ? AS keywords ON LOWER(student.text.content) = LOWER(keywords.b)\
           OR LOWER(student.lemma) = keywords.b WHERE student.text.content IS NULL AND keywords.b IS NOT NULL',
             [student, keywords]);
-       //console.log(keywords);
-        console.log(keyNotinAns)
 
         ansNotinKey = alasql('SELECT student.text.content as a,student.lemma as lemma \
          FROM ? AS student LEFT JOIN ? AS keywords ON LOWER(student.text.content) = LOWER(keywords.b)\
           OR LOWER(student.lemma) = keywords.b WHERE student.text.content IS NOT NULL AND keywords.b IS NULL',
             [student, keywords]);
-
-        console.log('check');
-        console.log(keyNotinAns);
+        if(ansNotinKey.length>0) {
+            for (var i = 0; i < ansNotinKey.length; i++) {
+                if (i == ansNotinKey.length - 1) {
+                    CallAPI(i, ansNotinKey[i].a, $http, 'no end');
+                }
+                else {
+                    CallAPI(i, ansNotinKey[i].a, $http, 'no end');
+                }
+            }
+        }
         if(keyNotinAns.length>0) {
             for (var i = 0; i < keyNotinAns.length; i++) {
                 if (i == keyNotinAns.length - 1) {
-                    console.log('a')
                     CallAPI(i, keyNotinAns[i], $http, 'end');
                 }
                 else {
-                    console.log('b')
                     CallAPI(i, keyNotinAns[i], $http, 'no end');
                 }
             }
@@ -79,17 +83,6 @@ function AnswerEval($scope,$http,qid){
         {
             Display();
         }
-        /*for(var i=0;i<ansNotinKey.length;i++)
-        {
-            /!*if(i==keyNotinAns.length-1)
-            {*!/
-            CallAPI(i,ansNotinKey[i],$http,'end');
-            /!* }
-             else
-             {*!/
-            // CallAPI(i,keyNotinAns[i],$http,'no end');
-            //}
-        }*/
     }
     var CallAPI=function(i,value,$http,isEnd)
     {
@@ -128,9 +121,6 @@ function AnswerEval($scope,$http,qid){
             });
     }
     function Display(){
-        //console.log(synonyms);
-        //console.log(ansNotinKey);
-        console.log('display');
         for(var i=0;i<keyNotinAns.length && keyNotinAns.length>0;i++) {
             //console.log('bbb'+synonyms.synonyms[keyNotinAns[i]]);
             if(synonyms.synonyms[keyNotinAns[i]] !=undefined) {
@@ -139,15 +129,33 @@ function AnswerEval($scope,$http,qid){
          OR LOWER(synonyms._) =LOWER(ansNotinKey.lemma)\
          WHERE LENGTH(ansNotinKey.a)>1',
                     [synonyms.synonyms[keyNotinAns[i]], ansNotinKey]);
-                console.log(foundinAns);
                 for (var j = 0; j < foundinAns.length; j++) {
                     if (keyNotinAns.length > 0) {
                         //console.log('aaa'+foundinAns[j].b);
                         var index = keyNotinAns.indexOf(foundinAns[j].b);
-                        console.log('remove '+keyNotinAns[index]);
                         //console.log(index);
                         if (index >= 0) {
                             i = i - 1;
+                            keyNotinAns.splice(index, 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        for(var i=0;i<ansNotinKey.length && keyNotinAns.length>0;i++) {
+            //console.log(synonyms.synonyms[ansNotinKey[i].a]);
+            if(synonyms.synonyms[ansNotinKey[i].a] !=undefined) {
+                foundinKey = alasql('SELECT DISTINCT synonyms._ as a,keyNotinAns._ as b,keyNotinAns._ as ans \
+         FROM ? AS synonyms JOIN ? AS keyNotinAns ON LOWER(synonyms._) LIKE "%"+' + 'LOWER(keyNotinAns._)' + '+"%" \
+         WHERE LENGTH(keyNotinAns._)>1',
+                    [synonyms.synonyms[ansNotinKey[i].a], keyNotinAns]);
+                for (var j = 0; j < foundinKey.length; j++) {
+                    if (keyNotinAns.length > 0) {
+                        //console.log('aaa'+foundinAns[j].b);
+                        var index = keyNotinAns.indexOf(foundinKey[j].b);
+                        //console.log(index);
+                        if (index >= 0) {
                             keyNotinAns.splice(index, 1);
                         }
                     }
@@ -173,19 +181,38 @@ function AnswerEval($scope,$http,qid){
          FROM ? AS Keywords JOIN ? AS keyNotinAns ON LOWER(keyNotinAns._) =LOWER(Keywords.b)\
          join ? keygrp on Keywords.a=keygrp.a',
             [keywords,keyNotinAns,keywordsgroup]);
+
+        var checkresult = alasql('SELECT result1.key,result1.score,result1.ans,result1.actvalue\
+         FROM ? AS result1 JOIN ? AS result2 ON LOWER(result1.ans) = LOWER(result2.ansvalue)\
+         where result1.actlLabel="NN" and result2.anslabel="NN" and LOWER(result1.ans+result1.actvalue)=LOWER(result2.ansvalue+result2.ans)',
+            [result, result]);
+
+        var swapKeys =alasql('SELECT DISTINCT keywords.a as a,keywords.b as b,keywordsgroup.score as score,keywordsgroup.[count] as `count` \
+         FROM ? AS checkresult JOIN ? AS keywordsgroup ON LOWER(checkresult.key) =LOWER(keywordsgroup.a)\
+         join ? AS keywords on LOWER(checkresult.ans)=LOWER(keywords.b) or LOWER(checkresult.actvalue)=LOWER(keywords.b)',
+            [checkresult,keywordsgroup,keywords]);
+
+        for(var i=0;i<swapKeys.length;i++) {
+            missingKeys.push({a: swapKeys[i].a, b: swapKeys[i].b, score: swapKeys[i].score, count: swapKeys[i].count});
+        }
+
+        
+
         var score=10;
-        console.log(missingKeys);
         for(var i=0;i<missingKeys.length;i++){
             score=score-(missingKeys[i].score/missingKeys[i]['count']);
         }
+
+        if(studentNegCount[0].count!=tutorNegCount[0].count)
+        {
+            score=(score<=2?0:score-2);
+        }
         score=score*10;
-        //console.log(score);
-        //console.log(Question.answer);
+
         if(score>50){
             $('.progress-bar-info').css('background-color', 'green');;
         }
-        else
-        {
+        else {
             $('.progress-bar-info').css('background-color', 'red');;
         }
         $('#txtanswer').text(useranswer);
